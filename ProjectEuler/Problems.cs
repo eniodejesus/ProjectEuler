ed91b349084bbc9472b50a75344ae5678e9852a8
Enio De Jesus <eniodejesus@gmail.com>
2014-10-22 00:23:28 -0400

    Start
    First 10 Problems from Project Eulers Page

---
+++ ProjectEuler/Problems.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace ProjectEuler
+{
+    public class Problems
+    {
+        int[,] Problem11Grid;
+ 
+        Int64 NextFibonacciNumber(Int64 n1, Int64 n2)
+        {
+            return n1 + n2;
+        }
+ 
+        Int64 DecomposeNumberInPrimeFactors(Int64 Number)
+        {
+ 
+            List<Int64> PrimeNumber = new List<Int64>();
+            PrimeNumber.Add(2);
+ 
+            while (Number > 1)
+            {
+                Number = GetNumberFromDiv(Number, PrimeNumber.LastOrDefault());
+                PrimeNumber.Add(GetNextPrimeNumber(PrimeNumber));
+            }
+ 
+            PrimeNumber.Remove(PrimeNumber.LastOrDefault());
+ 
+            return PrimeNumber.LastOrDefault();
+        }
+ 
+        Int64 GetNumberFromDiv(Int64 Upper, Int64 Lower)
+        {
+            while (Upper % Lower == 0)
+            {
+                Upper = Upper / Lower;
+            }
+            return Upper;
+        }
+ 
+        Int64 GetNextPrimeNumber(List<Int64> PrimeList)
+        {
+            Int64 currentPrime;
+            Int64 testPrime = 0;
+            bool pass = false;
+ 
+            currentPrime = PrimeList.LastOrDefault();
+            
+            while (!pass)
+            {
+                if (currentPrime == 2)
+                    testPrime = currentPrime + 1;
+                else
+                    testPrime = currentPrime + 2;
+ 
+                pass = true;
+                foreach (Int64 Prime in PrimeList)
+                {
+                    if (Prime > Math.Sqrt(testPrime))
+                        break;
+                    if (testPrime % Prime == 0)
+                    { pass = false; currentPrime = testPrime; break; }
+                }
+            }
+            return testPrime;
+ 
+        }
+ 
+        bool IsExactForAllNumbers(Int64 Number, Int64 Max)
+        {
+            while (Max > 1)
+            {
+                if (Number % Max != 0)
+                    return false;
+                Max -= 1;
+            }
+            return true;
+        }
+ 
+        bool isPalindrome(Int64 Number)
+        {
+            string numero = Number.ToString();
+ 
+            for (int i = 0; i < numero.Length; i++)
+            {
+                if (numero[i] != numero[numero.Length - i - 1])
+                    return false;
+            }
+            return true;
+ 
+                
+        }
+ 
+        Int64 GetSquareNumber(Int64 Number)
+        {
+            return Number * Number;
+        }
+ 
+        Int64 GridDown(int X, int Y)
+        {
+            if (X > 16) return 0;
+            Int64 Product = 1;
+            for (int i = 0; i < 4; i++)
+			{
+                Product *= Problem11Grid[X + i, Y];
+            }
+            return Product;
+        }
+        Int64 GridRigth(int X, int Y)
+        {
+            if (Y > 16) return 0;
+            Int64 Product = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                Product *= Problem11Grid[X, Y + i];
+            }
+            return Product;
+
+        }
+        Int64 GridDiagonal(int X, int Y)
+        {
+            if (X > 16 || Y > 16) return 0;
+            Int64 Product = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                Product *= Problem11Grid[X + i, Y + i];
+            }
+            return Product;
+        }
+        Int64 GridDiagonalBack(int X,int Y)
+        {
+            if (X > 16 || Y < 3) return 0;
+            Int64 Product = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                Product *= Problem11Grid[X + i, Y - i];
+            }
+            return Product;
+        }
+ 
+        Int64 DivisorAmount(Int64 Number)
+        {
+            Int64 count = 2;
+            Int64 Divisors = 2;
+            while (count < Math.Sqrt(Number))
+            {
+                if (Number % count == 0)
+                    Divisors++;
+                count++;
+            }
+            return Divisors*2;
+        }
+
+        public Int64 Problem1(Int64 topNumber)
+        {
+            Int64 sum=0;
+            for (int i = 0; i < topNumber; i++)
+			{
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+			}
+            return sum;
+        }
+ 
+        public Int64 Problem2(Int64 topNumber)
+        {
+            Int64 n1 = 0;
+            Int64 n2 = 1;
+            Int64 temp = NextFibonacciNumber(n1, n2);
+            Int64 sum = 0;
+ 
+            while (temp <= topNumber)
+            {
+                if (temp % 2 == 0)
+                    sum += temp;
+ 
+                n1 = n2;
+                n2 = temp;
+                temp = NextFibonacciNumber(n1, n2);
+            }
+ 
+            return sum;
+ 
+        }
+ 
+        public Int64 Problem3(Int64 Number)
+        {
+            return DecomposeNumberInPrimeFactors(Number);
+        }
+ 
+        public Int64 Problem4(Int64 Min , Int64 Max)
+        {
+            List<Int64> Palin = new List<Int64>();
+            Int64 result;
+            for (Int64 i = Min; i < Max; i++)
+            {
+                for (Int64 j = Min; j < Max; j++)
+                {
+                    result = i * j;
+                    if (isPalindrome(result))
+                        Palin.Add(result);
+                }
+            }
+            return Palin.Max();
+        }
+ 
+        public Int64 Problem5(Int64 Max)
+        {
+            Int64 Number = Max;
+            while (!IsExactForAllNumbers(Number,Max))
+            {
+                Number += Max;
+            }
+            return Number;
+        }
+ 
+        public Int64 Problem6(Int64 TopNumber)
+        {
+            Int64 sumOfSquare = 0;
+            Int64 squareOfSum = 0;
+ 
+            for (Int64 i = 1; i <= TopNumber; i++)
+            {
+                sumOfSquare += GetSquareNumber(i);
+                squareOfSum += i;
+            }
+            squareOfSum = GetSquareNumber(squareOfSum);
+ 
+            return  squareOfSum - sumOfSquare;
+        }
+ 
+        public Int64 Problem7(Int64 PrimeAmount)
+        {
+            List<Int64> PrimeNumber = new List<Int64>();
+            PrimeNumber.Add(2);
+            for (Int64 i = 1; i < PrimeAmount; i++)
+            {
+                PrimeNumber.Add(GetNextPrimeNumber(PrimeNumber));
+            }
+            return PrimeNumber.LastOrDefault();
+        }
+ 
+        public Int64 Problem8(string Digits, Int64 places)
+        {
+            Int64 Max = 0;
+            Int64 CurrentValue = 0;
+            int totalMoves = 0;
+            for (int i = 0; i <= Digits.Length-places-1; i++)
+            {
+                CurrentValue = 1;
+                for (int j = 0; j < places; j++)
+                {
+                    CurrentValue *= Int64.Parse(Digits[totalMoves+j].ToString());
+                }
+                totalMoves++;
+                Max = Math.Max(CurrentValue, Max);
+            }
+            return Max;
+        }
+ 
+        public Int64 Problem9(Int64 MaxSum)
+        {
+            for (Int64 x = 0; x < MaxSum; x++)
+            {
+                for (Int64 y = x+1; y < MaxSum; y++)
+                {
+                    for (Int64 z = y+1; z < MaxSum; z++)
+                    {
+                        if (x + y + z == MaxSum)
+                        {
+                            if (GetSquareNumber(x) + GetSquareNumber(y) == GetSquareNumber(z))
+                                return x * y * z;
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
+ 
+        public Int64 Problem10(Int64 MaxPrime)
+        {
+            List<Int64> PrimeNumber = new List<Int64>();
+            PrimeNumber.Add(2);
+            while(PrimeNumber.LastOrDefault() < MaxPrime)
+            {
+                PrimeNumber.Add(GetNextPrimeNumber(PrimeNumber));
+            }
+            PrimeNumber.Remove(PrimeNumber.LastOrDefault());
+            return PrimeNumber.Sum();
+            
+        }
+ 
+        public Int64 Problem11(int[,] Array)
+        {
+            Problem11Grid = Array;
+            Int64 Max = 0;
+            Int64 Tester = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    Tester = Math.Max(Math.Max(GridDown(i, j), GridRigth(i, j)), 
+                        Math.Max(GridDiagonal(i, j), GridDiagonalBack(i, j)));
+                    Max = Math.Max(Max, Tester);
+                    
+                }
+            }
+            return Max;
+        }
+
+        public Int64 Problem12(Int64 AmountOfDivisors)
+        {
+            Int64 count = 1;
+            Int64 sum = 0;
+            List<Int64> triangleNumbers = new List<Int64>();
+            while (true)
+            {
+                sum = triangleNumbers.LastOrDefault() + count;
+                triangleNumbers.Add(sum);
+                if (DivisorAmount(triangleNumbers.LastOrDefault()) >= AmountOfDivisors)
+                    return triangleNumbers.LastOrDefault();
+                ++count;
+            }
+        }
+    }
+}
---
+++ ProjectEulerTester/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ProjectEuler;
+
+namespace ProjectEulerTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProjectEuler.Problems Ejercicio = new Problems();
+            Constant Fixed = new Constant();
+
+            DateTime time;
+
+            time = DateTime.Now;
+            Console.WriteLine("Problem 1: {0}", Ejercicio.Problem1(1000));
+            Console.WriteLine("Time: {0}", (DateTime.Now - time).Milliseconds);
+
+            time = DateTime.Now;
+            Console.WriteLine("Problem 2: {0}", Ejercicio.Problem2(4000000));
+            Console.WriteLine("Time: {0}", (DateTime.Now - time).Milliseconds);
+
+            time = DateTime.Now;
+            Console.WriteLine("Problem 3: {0}", Ejercicio.Problem3(600851475143));
+            Console.WriteLine("Time: {0}", (DateTime.Now - time).Milliseconds);
+
+            time = DateTime.Now;
+            Console.WriteLine("Problem 4: {0}", Ejercicio.Problem4(100, 1000));
+            Console.WriteLine("Time: {0}", (DateTime.Now - time).Milliseconds);
+
+            time = DateTime.Now;
+            Console.WriteLine("Problem 5: {0}", Ejercicio.Problem5(20));
+            Console.WriteLine("Time: {0}", (DateTime.Now - time).Milliseconds);
+
+            time = DateTime.Now;
+            Console.WriteLine("Problem 6: {0}", Ejercicio.Problem6(100));
+            Console.WriteLine("Time: {0}", (DateTime.Now - time).Milliseconds);
+
+            time = DateTime.Now;
+            Console.WriteLine("Problem 7: {0}", Ejercicio.Problem7(10001));
+            Console.WriteLine("Time: {0}", (DateTime.Now - time).Milliseconds);
+
+            time = DateTime.Now;
+            Console.WriteLine("Problem 8: {0}", Ejercicio.Problem8(Fixed.Problem8String, 13));
+            Console.WriteLine("Time: {0}", (DateTime.Now - time).Milliseconds);
+
+            time = DateTime.Now;
+            Console.WriteLine("Problem 9: {0}", Ejercicio.Problem9(1000));
+            Console.WriteLine("Time: {0}", (DateTime.Now - time).Milliseconds);
+
+            time = DateTime.Now;
+            Console.WriteLine("Problem 10: {0}", Ejercicio.Problem10(2000000));
+            Console.WriteLine("Time: {0}", (DateTime.Now - time).Milliseconds);
+
+            time = DateTime.Now;
+            Console.WriteLine("Problem 11: {0}", Ejercicio.Problem11(Fixed.buildArrayProblem11()));
+            Console.WriteLine("Time: {0}", (DateTime.Now - time).Milliseconds);
+
+            time = DateTime.Now;
+            Console.WriteLine("Problem 12: {0}", Ejercicio.Problem12(500));
+            Console.WriteLine("Time: {0}", (DateTime.Now - time).Milliseconds);
+
+            Console.ReadKey();
+        }
+    }
+}
